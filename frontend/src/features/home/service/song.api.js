0133e356809cffa757604ba897223d4914c40ca6
Prixxe45 <prince45tri@gmail.com>
2026-05-18 02:08:29 +0530

    updatedlink

--- frontend/src/features/home/service/song.api.js
+++ frontend/src/features/home/service/song.api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://moodify-9pce.onrender.com/api',
 })
 
 export async function getSong({songFile, mood}) {
